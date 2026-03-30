fd8f0ca90582ef035bd0cbe11788a9c2987267cc
JuWal86 <131901946+JuWal86@users.noreply.github.com>
2026-03-31 00:02:39 +0200

    Delete measurement_programme directory

--- measurement_programme/tests/test_metrics_smoke.py
+++
@@ -1,6 +0,0 @@
-from measure.metrics.static_metrics import compute_static_repo_metrics
-
-def test_static_metrics_smoke():
-    m = compute_static_repo_metrics(["def x():\n  return 1\n"])
-    assert m.loc >= 1
-    assert m.cc_avg >= 0
